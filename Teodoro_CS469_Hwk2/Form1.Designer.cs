e26fc40634e3b1246f38867ae1cf3180c1d26c50
Troy Teodoro <teodoro@unlv.nevada.edu>
2022-02-05 16:50:07 -0800

    Created Form App

--- Teodoro_CS469_Hwk2/Form1.Designer.cs
+++ Teodoro_CS469_Hwk2/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.chooseImageButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.coordinateDisplay = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,6 +42,7 @@
             this.chooseImageButton.TabIndex = 0;
             this.chooseImageButton.Text = "Choose Image";
             this.chooseImageButton.UseVisualStyleBackColor = true;
+            this.chooseImageButton.Click += new System.EventHandler(this.chooseImageButton_Click);
             // 
             // pictureBox1
             // 
@@ -49,19 +51,29 @@
             this.pictureBox1.Size = new System.Drawing.Size(542, 309);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // coordinateDisplay
+            // 
+            this.coordinateDisplay.AutoSize = true;
+            this.coordinateDisplay.Location = new System.Drawing.Point(132, 12);
+            this.coordinateDisplay.Name = "coordinateDisplay";
+            this.coordinateDisplay.Size = new System.Drawing.Size(0, 13);
+            this.coordinateDisplay.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.coordinateDisplay);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.chooseImageButton);
             this.Name = "Form1";
             this.Text = "Teodoro CS469 - Image Zoom";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Button chooseImageButton;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label coordinateDisplay;
     }
 }
 
--- Teodoro_CS469_Hwk2/Form1.cs
+++ Teodoro_CS469_Hwk2/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -17,9 +18,51 @@ namespace Teodoro_CS469_Hwk2
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private Bitmap getColorValue(Bitmap original)
         {
-
+            try
+            {
+                Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+                for (int i = 0; i < original.Width; i++)
+                {
+                    for (int j = 0; j < original.Height; j++)
+                    {
+                        //get the pixels from the original image
+                        Color originalColor = original.GetPixel(i, j);
+                        //create the gray scale version of each pixel
+                        int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+                        Color newColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                        newBitmap.SetPixel(i, j, newColor);
+                    }
+                }
+                return newBitmap;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
         }
+
+        private void chooseImageButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog imagefileopen = new OpenFileDialog();
+            imagefileopen.Filter = "Image Files(*.jpg;*.jpeg; *.gif; *.bmp; *.png)|*.jpg;*.jpeg; *.gif; *.bmp ; *.png";
+            if (imagefileopen.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(imagefileopen.FileName);
+                pictureBox1.Size = pictureBox1.Image.Size;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coordinates = me.Location;
+            Debug.WriteLine("Output coordinates:{0}, {1}\n", coordinates.X, coordinates.Y);
+            coordinateDisplay.Text = coordinates.ToString();
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+        
     }
 }
